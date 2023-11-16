f235cca20f365bec14af92884595c5561cb8a022
kiri <114994914+kirilft@users.noreply.github.com>
2023-11-16 22:27:51 +0100

    uwu update

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -36,19 +36,20 @@ export class MainComponent implements OnInit, AfterViewInit {
   blinkCaretState: string = 'initial';
 
   sentences: string[] = [
-    "Imagination is more important than knowledge. - Albert Einstein",
-    "Nothing in life is to be feared, it is only to be understood. - Marie Curie",
-    "Innovation distinguishes between a leader and a follower. - Steve Jobs",
-    "Education is the most powerful weapon which you can use to change the world. - Nelson Mandela",
-    "That brain of mine is something more than merely mortal; as time will show. - Ada Lovelace",
-    "Learning never exhausts the mind. - Leonardo da Vinci",
-    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
-    "If I have seen further it is by standing on the shoulders of giants. - Isaac Newton",
-    "Science and everyday life cannot and should not be separated. - Rosalind Franklin",
-    "Intelligence is the ability to adapt to change. - Stephen Hawking",
     "Pants are for losers. - Zoe McFife",
-    "Be gay do crimes. - Alexander Hamilton"
+    "Be gay do crimes. - Alexander Hamilton",
+    "Reality is just a crutch for people who can't handle science fiction. - Skylar Astin",
+    "Procrastination is the art of keeping up with yesterday. - Don Marquis",
+    "I’m not arguing, I’m just explaining why I’m right. - Charlie Bright",
+    "Always borrow money from a pessimist, they never expect it back. - Oscar Wilde",
+    "I don’t have a dirty mind, I have a sexy imagination. - Noah Sparks",
+    "Why do they call it rush hour when nothing moves? - Robin Williams",
+    "I'm not lazy, I'm on energy-saving mode. - Leah Clarkson",
+    "The early bird might get the worm, but the second mouse gets the cheese. - Steven Wright",
+    "Life is short. Smile while you still have teeth. - Mallory Hopkins",
+    "I’m not weird, I’m a limited edition. - Sam Cawthorn"
   ];
+  
   
   ngOnInit(): void {
     this.displayRandomSentence();
