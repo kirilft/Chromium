f59f9571e6c591a99365683c5a838ff5cd229664
Kiri <114994914+kirilft@users.noreply.github.com>
2024-03-18 07:36:14 +0100

    added components

---
+++ src/app/backend/backend.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-backend',
+  standalone: true,
+  imports: [],
+  templateUrl: './backend.component.html',
+  styleUrl: './backend.component.css'
+})
+export class BackendComponent {
+
+}
