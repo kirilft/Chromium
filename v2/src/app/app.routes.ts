3db68868cdfdb2bac84d989f0962ee4cbdbeb104
Kiri <114994914+kirilft@users.noreply.github.com>
2024-04-05 09:23:44 +0200

    header done

--- v2/src/app/app.routes.ts
+++ v2/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component'; // Import the MainComponent
 
 export const routes: Routes = [
-  { path: 'main', component: MainComponent }, // Set up the 'main' route
-  { path: '', redirectTo: '/main', pathMatch: 'full' } // Optional: Redirect the default route to 'main'
+  { path: '', component: MainComponent }, // Set up the 'main' route
+  { path: 'main', redirectTo: '', pathMatch: 'full' } // Optional: Redirect the default route to 'main'
 ];
