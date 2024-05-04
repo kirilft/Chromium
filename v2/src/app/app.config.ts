ae3dc84526c15b0ead6959dd00bd5fb34899fd85
Kiri <114994914+kirilft@users.noreply.github.com>
2024-05-05 00:04:35 +0200

    test

--- v2/src/app/app.config.ts
+++ v2/src/app/app.config.ts
@@ -12,5 +12,8 @@ export const appConfig: ApplicationConfig = {
     }), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
+    }), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
     })]
 };
