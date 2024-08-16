e8232799d1e3762d4222894a4451166a12c2bd54
Kiri <114994914+kirilft@users.noreply.github.com>
2024-08-16 02:06:28 +0200

    header HTML

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
-import { OddityComponent } from './oddity/oddity.component';
+
 
 export const routes: Routes = [
   { path: 'main', component: MainComponent }, // Changed from redirect to component
-  { path: 'oddity', component: OddityComponent },
   { path: '', redirectTo: 'main', pathMatch: 'full' } // Changed from component to redirect
 ];
