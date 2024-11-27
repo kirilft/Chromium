5c9c0175e0c65ae5a46b61878657532ea975eb97
Kiri <114994914+kirilft@users.noreply.github.com>
2024-11-27 23:04:53 +0100

    cleaning

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 // Server.ts
 import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr';
+import { CommonEngine } from '@angular/ssr/node';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
