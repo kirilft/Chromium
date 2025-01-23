293258e47781494622723f3ffa78e72ac9d776ef
Kiri <114994914+kirilft@users.noreply.github.com>
2025-01-23 17:11:26 +0100

    adding pirates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './Pages/main/main.component';
 import { PageNotFoundComponent } from './Pages/page-not-found/page-not-found.component';
 import { ArcComponent } from './Pages/arc/arc.component';
+import { Zoe27Component } from './Pages/zoe27/zoe27.component';
 
 export const routes: Routes = [
   {
@@ -18,6 +19,10 @@ export const routes: Routes = [
   {
     path: 'arc',
     component: ArcComponent
+  },
+  {
+    path: 'zoe27',
+    component: Zoe27Component
   },
   {
     path: '**',
---
+++ src/app/zoe27/zoe27.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zoe27',
+  imports: [],
+  templateUrl: './zoe27.component.html',
+  styleUrl: './zoe27.component.css'
+})
+export class Zoe27Component {
+
+}
