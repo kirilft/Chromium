5dd407fa511c2e83ea4aa051df5af9e4a71745c0
Kiri <114994914+kirilft@users.noreply.github.com>
2025-06-04 10:14:55 +0200

    test(theme): verify toggleTheme emission

--- src/app/services/theme.service.spec.ts
+++ src/app/services/theme.service.spec.ts
@@ -2,16 +2,33 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ThemeService } from './theme.service';
+import { ConsentService } from './consent.service';
 
 describe('ThemeService', () => {
   let service: ThemeService;
+  let consentServiceMock: jasmine.SpyObj<ConsentService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    consentServiceMock = jasmine.createSpyObj('ConsentService', ['hasConsent']);
+    consentServiceMock.hasConsent.and.returnValue(false);
+    TestBed.configureTestingModule({
+      providers: [{ provide: ConsentService, useValue: consentServiceMock }],
+    });
     service = TestBed.inject(ThemeService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should toggle theme and emit change', () => {
+    const initialTheme = service.currentTheme;
+    let emittedTheme: 'light' | 'dark' | undefined;
+    service.themeChanged.subscribe((theme) => (emittedTheme = theme));
+
+    service.toggleTheme();
+
+    expect(service.currentTheme).not.toBe(initialTheme);
+    expect(emittedTheme).toBe(service.currentTheme);
   });
 });
